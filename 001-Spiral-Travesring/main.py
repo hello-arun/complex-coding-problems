795e923f4c4ed07a25582a204cb76949f3701f69
arunjangir <jangirarun786@gmail.com>
2022-03-21 22:34:25 +0300

    Rakesh Solution Spiral

--- 001-Spiral-Travesring/main.py
+++ 001-Spiral-Travesring/main.py
@@ -1,38 +1,38 @@
-# Define Matrix Dimensions Here
-_R = 5
-_C = 5
+def spiralTraverse(a, n, m):
+    top = 0
+    down = n-1
+    left = 0
+    right = m-1
+    dir = 0
+    res = []
 
-# Do not edit just run and see the output
-R = _R
-C = _C
+    while(top <= down and left <= right):
+        # if(dir == 0):
+        for i in range(left, right+1):
+            res.append(a[top][i])
+        top += 1
+    # elif(dir == 1):
+        for i in range(top, down+1):
+            res.append(a[i][right])
+        right -= 1
+    # elif(dir == 2):
+        for i in range(right, left-1, -1):
+            res.append(a[down][i])
+        down -= 1
+    # elif(dir == 3):
+        for i in range(down, top-1, -1):
+            res.append(a[i][left])
+        left += 1
 
-cr = 0
-cc = -1
+        # dir = (dir+1) % 4
+    return(res)
 
-dr = -1
-dc = 1
 
-# Wether pointer is moving along column or along rows
-# 0 => false
-# 1 => true
-movingAlongColumn = 0
-steps = C
-
-while(C > 0 and R > 0):
-    # print(steps)
-    for i in range(steps):
-        cr += dr*movingAlongColumn
-        cc += dc*(1-movingAlongColumn)
-        print(1+cr*_C+cc, end=" ")
-    if movingAlongColumn:
-        movingAlongColumn = 0
-        if C > 0:
-            C -= 1
-            steps=C
-        dc *= -1
-    else:
-        movingAlongColumn = 1
-        if R > 0:
-            R -= 1
-            steps=R
-        dr *= -1
+# Driver Code
+a = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+    [17, 18, 19, 20]]
+print(spiralTraverse(a, len(a), len(a[0])))
