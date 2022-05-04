743f82839b40c4b95a664699c5d1126c10706402
arunjangir <jangirarun786@gmail.com>
2022-05-04 23:27:12 +0300

    direct and fft method implemented

--- 901-FFT/main.py
+++ 901-FFT/main.py
@@ -1,14 +1,17 @@
 from math import sin, cos, pi
-
+from timeit import timeit
 # Recursive function of FFT
+
+
 def fft(a):
     n = len(a)
     # if input contains just one element
     if n == 1:
         return [a[0]]
     # For storing n complex nth roots of unity
-    theta = -2*pi/n
+    theta = 2*pi/n
     w = list(complex(cos(theta*i), sin(theta*i)) for i in range(n))
+    # print(w)
     # Separate coefficients
     Aeven = a[0::2]
     Aodd = a[1::2]
@@ -27,12 +30,31 @@ def fft(a):
     return Y
 
 
+def direct(a):
+    # convert from a0+a1*x+a2*xx+a3*xxx => a3*xxx+a2*xx+a1*x+a0
+    a = a[::-1]
+    n = len(a)
+    Y = [0]*n
+    theta = 2*pi/n
+    n_roots = list(complex(cos(theta*i), sin(theta*i)) for i in range(n))
+    for i, root in enumerate(n_roots):
+        result = a[0]
+        # Evaluate value of polynomial using Horner's method
+        for i in range(1, n):
+            result = result*root + a[i]
+        Y[i] = result
+    return Y
+
+
 # Driver code
 if __name__ == '__main__':
     # Poly a_n(x^n) => n range from 0 to n-1
-    a = [1, 2, 3, 4, 5]
-    print(a[::2])
-    print(a[1::2])
-    b = fft(a)
-    for B in b:
-        print(B)
+    a = [1]*512
+    time_direct = timeit(lambda: direct(a), number=10000)
+    time_fft = timeit(lambda: fft(a), number=10000)
+    print(f"Time for direct evaluation : {time_direct:0.5f}")
+    print(f"Time for fastFT evaluation : {time_fft:0.5f}")
+    # for value in values_fft:
+    # print(value)
+    # for value in values_direct:
+    # print(value)
